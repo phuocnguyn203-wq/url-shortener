0f01f5606fc7d32bb4ec13547044851e8a4f2afc
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-01 16:33:32 +0700

    handle authenticate error in authenticate middleware

--- src/app/middlewares/authenticate.middleware.js
+++ src/app/middlewares/authenticate.middleware.js
@@ -1,11 +1,28 @@
 import { verifyJwtToken } from "../services/users.service.js";
+import jwt from "jsonwebtoken";
 
 export function authenticate(req, res, next) {
   const jwtToken = req.cookies.token;
   if (!jwtToken) {
-    return res.status(401).json({ error: "Missing Token " });
+    return res.status(401).json({ error: "Missing Token" });
   }
-  const userId = verifyJwtToken(jwtToken, process.env.JWT_SECRET)["sub"];
-  req.userId = Number(userId);
+  /*
+  Handle error here, because not like DataAccessError,
+  it scatters many places, it should have a centralized
+  place to handle errors from sources. But this authenticate
+  middleware is only source for error, so handling error here
+  is best place
+  */
+  try {
+    const userId = verifyJwtToken(jwtToken, process.env.JWT_SECRET)["sub"];
+    req.userId = Number(userId);
+  } catch (error) {
+    if (
+      error instanceof jwt.TokenExpiredError ||
+      error instanceof jwt.JsonWebTokenError
+    )
+      return res.status(404).json({ error: "404" });
+  }
+
   next();
 }
