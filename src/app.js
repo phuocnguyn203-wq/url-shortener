99f891c5cfc311aaa03562968efdb024a33daec3
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-08-11 00:26:14 +0700

    exchange name for main and app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
-import app from "./main.js";
+import express from "express";
+import errorHandler from "./app/middlewares/errorHandler.middleware.js";
+import cookieParser from "cookie-parser";
 
-const PORT = 3000;
+import shortenerRouter from "./app/routes/shortener.route.js";
+import userRouter from "./app/routes/users.route.js";
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-})
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/shortened", shortenerRouter);
+app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("hello, world");
+});
+
+app.use(errorHandler);
+
+export default app;
+
--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
-import express from "express";
-import errorHandler from "./app/middlewares/errorHandler.middleware.js";
-import cookieParser from "cookie-parser";
+import app from "./app";
 
-import shortenerRouter from "./app/routes/shortener.route.js";
-import userRouter from "./app/routes/users.route.js";
+const PORT = 3000;
 
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
-app.set("view engine", "ejs");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use("/shortened", shortenerRouter);
-app.use("/users", userRouter);
-
-app.get("/", (req, res) => {
-  res.send("hello, world");
-});
-
-app.use(errorHandler);
-
-export default app;
-
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
